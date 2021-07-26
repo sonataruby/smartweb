a95b7f9a66b8d21cc9dd881e101f57846ebb591d
Roller Davic <rollerdavic@gmail.com>
2021-07-26 09:53:55 +0700

    Create permission.php

---
+++ templates/account/permission.php
@@ -0,0 +1,7 @@
+<?php echo $this->extend($layout); ?>
+
+<?php echo $this->section('body') ?>
+	<h3>Your Have Not Access</h3>
+	<a href="/" class="btn btn-primary btn-sm">Back To Home</a>
+
+<?php echo $this->endSection() ?>
