d8fa8f43917cacacf7a28d2d23ddfad7d1751e6e
Roller Davic <rollerdavic@gmail.com>
2021-07-26 09:56:53 +0700

    Update permission.php

--- templates/account/permission.php
+++ templates/account/permission.php
@@ -1,7 +1,8 @@
 <?php echo $this->extend($layout); ?>
 
 <?php echo $this->section('body') ?>
+<div class="container">
 	<h3>Your Have Not Access</h3>
 	<a href="/" class="btn btn-primary btn-sm">Back To Home</a>
-
+</div>
 <?php echo $this->endSection() ?>
