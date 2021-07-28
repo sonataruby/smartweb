662712a9b4568061847aaabfd52eecaeb6e7de90
Roller Davic <rollerdavic@gmail.com>
2021-07-28 21:00:30 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -21,7 +21,7 @@ $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Dashboard');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
-$routes->set404Override('Dashboard::show404');
+$routes->set404Override();
 $routes->setAutoRoute(true);
 
 /*
